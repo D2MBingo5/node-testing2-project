04e7f56bffbcd3f13ef5097d672583dc9e9eb57f
Leu Family <romanianfobjr@gmail.com>
2021-09-16 17:31:58 -0500

    scaffolding 2

--- api/server.js
+++ api/server.js
@@ -9,13 +9,22 @@ server.get('/', (req, res) => {
 })
 
 server.get('/fish', (req, res) => {
-    Fish.getAll()
-        .then(fish => {
-            res.status(200).json(fish)
-        })
-        .catch(err => {
-            res.status(500).json(error)
-        })
+    // Fish.getAll()
+    //     .then(fish => {
+    //         res.status(200).json(fish)
+    //     })
+    //     .catch(err => {
+    //         res.status(500).json(error)
+    //     })
+    res.end()
+})
+
+server.post('/fish', (req, res) => {
+    res.end()
+})
+
+server.delete('/fish/:id', (req, res) => {
+    res.end()
 })
 
 module.exports = server
