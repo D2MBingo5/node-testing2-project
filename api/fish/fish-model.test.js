5dd72087f742183be1249a95a2d20667a333f4cd
Leu Family <romanianfobjr@gmail.com>
2021-09-17 01:22:59 -0500

    getAll test 2 complete

--- api/fish/fish-model.test.js
+++ api/fish/fish-model.test.js
@@ -14,11 +14,19 @@ beforeEach(async () => {
 })
 
 describe('Fish db access functions', () => {
+
     describe('Fish.getAll', () => {
         it('resolves to all fish in the fish table',
         async () => {
             const fishes = await Fish.getAll()
             expect(fishes).toHaveLength(4)
         })
+        it('resolves to the correct fish shapes', async () => {
+            const fishes = await Fish.getAll()
+            expect(fishes[0]).toHaveProperty('id', 1)
+            expect(fishes[0]).toHaveProperty('species', 'red')
+            expect(fishes[1]).toMatchObject({ id: 2, species: 'blue' })
+        })
     })
+    
 })
